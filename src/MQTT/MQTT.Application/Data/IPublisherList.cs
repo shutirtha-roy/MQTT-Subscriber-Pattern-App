85e9ea6fdb63be2d5beaf8f36382ec304a193939
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-25 13:40:11 +0600

    Required properties and methods added for PublisherList

--- src/MQTT/MQTT.Application/Data/IPublisherList.cs
+++ src/MQTT/MQTT.Application/Data/IPublisherList.cs
@@ -1,5 +1,10 @@
-﻿namespace MQTT.Application.Data;
+﻿using MQTTnet.Client;
+
+namespace MQTT.Application.Data;
 
 public interface IPublisherList
 {
+    IDictionary<string, IList<IMqttClient>> PublisherItems { get; }
+    Task AddPublisher(string topic, IMqttClient client);
+    Task RemovePublisher(string topic, IMqttClient client);
 }
--- src/MQTT/MQTT.Application/Data/PublisherList.cs
+++ src/MQTT/MQTT.Application/Data/PublisherList.cs
@@ -1,5 +1,34 @@
-﻿namespace MQTT.Application.Data;
+﻿using MQTTnet.Client;
+
+namespace MQTT.Application.Data;
 
 public sealed class PublisherList : IPublisherList
 {
+    public IDictionary<string, IList<IMqttClient>> PublisherItems { get; private set; }
+
+    public PublisherList()
+    {
+        if (PublisherItems is null)
+            PublisherItems = new Dictionary<string, IList<IMqttClient>>();
+    }
+
+    public async Task AddPublisher(string topic, IMqttClient client)
+    {
+        if (!PublisherItems.ContainsKey(topic))
+        {
+            PublisherItems.Add(topic, new List<IMqttClient>());
+        }
+
+        PublisherItems[topic].Add(client);
+    }
+
+    public async Task RemovePublisher(string topic, IMqttClient client)
+    {
+        if (!PublisherItems.ContainsKey(topic))
+        {
+            throw new KeyNotFoundException("Publisher Not found");
+        }
+
+        PublisherItems[topic].Remove(client);
+    }
 }
